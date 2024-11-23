4266b5974712a3240747bdb6b3a324472ee597d1
melokn <gabriel.veloso5@gmail.com>
2024-11-23 20:43:07 -0300

    deploying api

--- src/server.ts
+++ src/server.ts
@@ -57,6 +57,8 @@ app.register(deleteHint)
 app.register(ping)
 
 
-app.listen(env.PORT, () => {
+app.listen({
+  port: env.PORT,
+}).then(() => {
   console.log(`Server running on port ${env.PORT}`);
-});
+})
